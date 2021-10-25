fd6428de99fead7497216052bd9802e914ffdce2
Aleksey <uscelemov1990@gmail.com>
2021-10-25 17:47:28 +0300

    lection_4

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'authapp',
     'mainapp',
 ]
 
@@ -71,6 +72,9 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'geekshop.wsgi.application'
+
+
+AUTH_USER_MODEL = 'authapp.ShopUser'
 
 
 # Database
--- mainapp/models.py
+++ mainapp/models.py
@@ -25,3 +25,7 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.name} ({self.category.name})'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
