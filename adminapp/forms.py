e2749ce439511ff37547358e48c3c2f2de4d7464
Aleksey <uscelemov1990@gmail.com>
2021-11-14 19:26:11 +0300

    lection_8

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -1,5 +1,7 @@
 from authapp.forms import ShopUserEditForm
 from authapp.models import ShopUser
+from django import forms
+from mainapp.models import Product
 
 
 class ShopUserAdminEditForm(ShopUserEditForm):
@@ -8,3 +10,15 @@ class ShopUserAdminEditForm(ShopUserEditForm):
         fields = '__all__'
 
 
+class ProductEditForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        #fields = '__all__'
+        exclude = ('is_active',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
